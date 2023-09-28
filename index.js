eef7431cf16e4db67d19b616a081f9e630b7591a
tariq aljadayah <tariqaljadayah@tariqs-MacBook-Pro.local>
2023-09-28 19:44:04 -0400

    first commit

--- index.js
+++ index.js
@@ -1,85 +1,71 @@
-const SERVER_NAME = 'product-api';
-const PORT = 5000;
-const HOST = '127.0.0.1';
+const API_SERVER_NAME = 'my-product-api';
+const API_PORT = 5000;
+const API_HOST = '127.0.0.1';
 
-var COUNTER_GET = 0;
-var COUNTER_POST = 0;
+let GET_REQUEST_COUNT = 0;
+let POST_REQUEST_COUNT = 0;
 
-var restify = require('restify')
+const restifyServer = require('restify');
+const ProductsDatabase = require('save')('products');
 
-    // Get a persistence engine for the products
-    , ProductsSave = require('save')('products')
+const apiServer = restifyServer.createServer({
+  name: API_SERVER_NAME,
+});
 
-    // Create the restify server
-    , server = restify.createServer({name: SERVER_NAME})
+apiServer.use(restifyServer.plugins.fullResponse()).use(restifyServer.plugins.bodyParser());
 
-server.listen(PORT, HOST, function () {
-    console.log('Server %s listening at %s', server.name, server.url)
-    console.log('Endpoints: %s/products', server.url)
-    console.log('Method: GET, POST,Delete')
-    console.log('test')
+apiServer.get('/products', (request, response, next) => {
+  console.log('- GET Products: Received Request');
+  GET_REQUEST_COUNT++;
 
-})
-
-server.use(restify.fullResponse()).use(restify.bodyParser())
-
-// Get all products
-server.get('/products', function (req, res, next) {
-    console.log('- GET Products: Received Request')
-    COUNTER_GET++;
-
-    ProductsSave.find({}, function (error, products) {
-        console.log(products)
-        if(products.length === 0){
-            res.send(204,products)
-        } else {
-            res.send(200,products)
-        }
-        console.log('- GET Products: Sending Response')
-    })
-
-    console.log('Processed Request Count--> GET: %s, POST: %s', COUNTER_GET, COUNTER_POST)
-})
-
-
-//Create Product
-server.post('/products', function (req, res, next) {
-    console.log('- Post Product: Received Request')
-    COUNTER_POST++;
-
-    if (req.params.productId === undefined) {
-        return next(new restify.InvalidArgumentError('ProductID Missing'))
+  ProductsDatabase.find({}, (error, products) => {
+    console.log(products);
+    if (products.length === 0) {
+      response.send(204, products);
+    } else {
+      response.send(200, products);
     }
-    if (req.params.name === undefined) {
-        return next(new restify.InvalidArgumentError('Product Name Missing'))
-    }
-    if (req.params.price === undefined) {
-        return next(new restify.InvalidArgumentError('Product Price Missing'))
-    }
-    if (req.params.quantity === undefined) {
-        return next(new restify.InvalidArgumentError('Product quantity Missing'))
-    }
-    var product = {
-        productId: req.params.productId,
-        name: req.params.name,
-        price: req.params.price,
-        quantity: req.params.quantity
-    }
+    console.log('- GET Products: Sending Response');
+  });
 
-    // Create product
-    ProductsSave.create(product, function (error, product) {
-        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-        res.send(201, product)
-        console.log('< Products POST: sending response')
-    })
-    console.log('Processed Request Count--> GET: %s, POST: %s', COUNTER_GET, COUNTER_POST)
-})
+  console.log('Processed Request Count--> GET: %s, POST: %s', GET_REQUEST_COUNT, POST_REQUEST_COUNT);
+});
 
-// Delete all products
-server.del('/products', function (req, res, next) {
-    ProductsSave.deleteMany({}, function (error, product) {
-        // If there are any errors, pass them to next in the correct format
-        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-        res.send()
-    })
-})
+apiServer.post('/products', (request, response, next) => {
+  console.log('- Post Product: Received Request');
+  POST_REQUEST_COUNT++;
+
+  const { productCode, productName, productPrice, productQuantity } = request.params;
+
+  if (!productCode || !productName || !productPrice || !productQuantity) {
+    return next(new restifyServer.errors.InvalidArgumentError('Missing product details'));
+  }
+
+  const product = {
+    productCode,
+    productName,
+    productPrice,
+    productQuantity,
+  };
+
+  ProductsDatabase.create(product, (error, savedProduct) => {
+    if (error) return next(new restifyServer.errors.InvalidArgumentError(JSON.stringify(error.errors)));
+    response.send(201, savedProduct);
+    console.log('< Products POST: sending response');
+  });
+
+  console.log('Processed Request Count--> GET: %s, POST: %s', GET_REQUEST_COUNT, POST_REQUEST_COUNT);
+});
+
+apiServer.del('/products', (request, response, next) => {
+  ProductsDatabase.deleteMany({}, (error, product) => {
+    if (error) return next(new restifyServer.errors.InvalidArgumentError(JSON.stringify(error.errors)));
+    response.send();
+  });
+});
+
+apiServer.listen(API_PORT, API_HOST, () => {
+  console.log(`Server ${apiServer.name} listening at ${apiServer.url}`);
+  console.log(`Endpoints: ${apiServer.url}/products`);
+  console.log('HTTP Methods: GET, POST, DELETE');
+});
